6eb6d5b69b76ae67a1f8f943390695194bd8851f
Felix Zedén Yverås <felix@fzy.se>
2022-06-19 14:51:54 +0200

    Update favicon
    The portal should use the dansdata favicon.
    
    This commit adds the favicon to the default document rendered by
    nextjs. This reduces the amount of copy-paste when new pages are
    added in the future.
    
    The html `lang` attribute is ignored in this commit as I have not
    yet decided on a render strategy. Hopefully nextjs will manage
    this for us but that is as of yet not confirmed.
    
    See also https://nextjs.org/docs/advanced-features/custom-document

---
+++ pages/_document.tsx
@@ -0,0 +1,57 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta charSet="UTF-8" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png?v=1"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png?v=1"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png?v=1"
+        />
+        <link
+          rel="manifest"
+          href="/site.webmanifest?v=1"
+        />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg?v=1"
+          color="#006877"
+        />
+        <link
+          rel="shortcut icon"
+          href="/favicon.ico?v=1"
+        />
+        <meta
+          name="msapplication-TileColor"
+          content="#006877"
+        />
+        <meta
+          name="theme-color"
+          content="#006877"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
