5f59d1621099eb9ca1b0e1ad72fc5d458254b609
Felix Zedén Yverås <felix@fzy.se>
2022-06-19 14:40:40 +0200

    Configure basic i18n
    The dansdata portal will be available in swedish and english
    (defaulting to swedish).
    
    This commit follows the guides on
    * https://nextjs.org/docs/advanced-features/i18n-routing
    * https://github.com/isaachinman/next-i18next
    
    to enable support for this.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import "../styles/globals.css";
+import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
