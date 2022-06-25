390bafa10eeef3c0a9ac7ce9ce46d10d7300412d
Felix Zedén Yverås <felix@fzy.se>
2022-06-25 03:58:08 +0200

    Remove div wrapper from storybook preview
    This div was initially introduced to help make text visible when
    toggling light/dark mode in storybook. The actual result was that some
    things became harder to see while desigining components. Therefore, this
    wrapper is now removed.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -26,9 +26,7 @@ export const parameters = {
 export const decorators = [
   (Story) => (
     <I18nextProvider i18n={i18n}>
-      <div className="bg-background text-on-background">
-        <Story />
-      </div>
+      <Story />
     </I18nextProvider>
   ),
 ];
