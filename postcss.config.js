e671ceda11fb32729b6d111c9147d6cde14c7900
Felix Zedén Yverås <felix@fzy.se>
2022-06-25 16:48:15 +0200

    Update css structure to follow guidelines
    Improve css structure to better follow the guidelines at
    https://tailwindcss.com/docs/adding-custom-styles#using-multiple-css-files

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
     "postcss-import": {},
+    "tailwindcss/nesting": {},
     // Ensure postcss-each is run before tailwindcss.
     // See https://github.com/madyankin/postcss-each/issues/39
     "postcss-each": {
