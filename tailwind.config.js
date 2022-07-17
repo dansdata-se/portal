680cef0d15dfedbeb4902157547d1ef38dc55f1e
Felix Zedén Yverås <felix@fzy.se>
2022-07-17 03:19:42 +0200

    Make `.container` center content and ensure padding for all sizes
    Tailwind tends to not apply enough spacing on small screens when using
    `.container`. This commit sets up some (IMO) sensible defaults.
    
    See also https://tailwindcss.com/docs/container#customizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,16 @@ module.exports = {
     "./styles/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     boxShadow: {
       ...elevation.theme.boxShadow,
     },
