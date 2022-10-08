763261c14fb70dcbfa0104dd720cbbace8ea3834
Felix Zedén Yverås <felix@fzy.se>
2022-10-08 22:33:20 +0000

    Add support for API tokens
    While supabase supports authentication out of the box, it does not
    support the generation of API key. After searching for quite a bit,
    I opened a feature request with Supabase
    (https://github.com/supabase/supabase/issues/7186) and made a deep dive
    into Supabase, PostgREST, etc. to come up with the solution in this
    commit.
    
    The migration is written in an article-like fashion to try and see if I
    liked such writing. The finished code was also released on
    https://gist.github.com/FelixZY/0aef530690458b381b8100afa19202c8.
    
    It should be noted that no environments beyond "local" has been set up
    yet, since I have not yet set up any remote supabase environments for
    this project. Placeholders are therefore left in place for now.

---
+++ supabase/seed.sql
@@ -0,0 +1,212 @@
+-- Create test users
+-- email: alice@example.com password: password
+-- email: bob@example.com password: password
+INSERT INTO auth.audit_log_entries (
+  instance_id,
+  id,
+  payload,
+  created_at)
+VALUES (
+  '00000000-0000-0000-0000-000000000000',
+  '1428b495-a59e-49ae-ad59-e061f7b2ca42',
+  '{"action":"user_signedup","actor_id":"f79a5ba6-7eaf-400e-8f70-c1e8cbd9631b","actor_username":"alice@example.com","log_type":"team","timestamp":"2022-08-05T14:06:38Z","traits":{"provider":"email"}}',
+  '2022-08-05 14:06:38.55586+00');
+
+INSERT INTO auth.audit_log_entries (
+  instance_id,
+  id,
+  payload,
+  created_at)
+VALUES (
+  '00000000-0000-0000-0000-000000000000',
+  '41e018de-2791-4990-bb31-1a85b432409c',
+  '{"action":"login","actor_id":"f79a5ba6-7eaf-400e-8f70-c1e8cbd9631b","actor_username":"alice@example.com","log_type":"account","timestamp":"2022-08-05T14:06:38Z","traits":{"provider":"email"}}',
+  '2022-08-05 14:06:38.561871+00');
+
+INSERT INTO auth.audit_log_entries (
+  instance_id,
+  id,
+  payload,
+  created_at)
+VALUES (
+  '00000000-0000-0000-0000-000000000000',
+  '21b3bcee-50f3-4cac-8bbe-7d9ebac7dd8b',
+  '{"action":"user_signedup","actor_id":"1912ff43-e876-4961-9631-ecd09fe019bb","actor_username":"bob@example.com","log_type":"team","timestamp":"2022-08-05T14:06:47Z","traits":{"provider":"email"}}',
+  '2022-08-05 14:06:47.337326+00');
+
+INSERT INTO auth.audit_log_entries (
+  instance_id,
+  id,
+  payload,
+  created_at)
+VALUES (
+  '00000000-0000-0000-0000-000000000000',
+  '1c45fe3a-6493-4d06-8acb-d646c902919e',
+  '{"action":"login","actor_id":"1912ff43-e876-4961-9631-ecd09fe019bb","actor_username":"bob@example.com","log_type":"account","timestamp":"2022-08-05T14:06:47Z","traits":{"provider":"email"}}',
+  '2022-08-05 14:06:47.340904+00');
+
+INSERT INTO auth.users (
+  instance_id,
+  id,
+  aud,
+  ROLE,
+  email,
+  encrypted_password,
+  email_confirmed_at,
+  invited_at,
+  confirmation_token,
+  confirmation_sent_at,
+  recovery_token,
+  recovery_sent_at,
+  email_change_token_new,
+  email_change,
+  email_change_sent_at,
+  last_sign_in_at,
+  raw_app_meta_data,
+  raw_user_meta_data,
+  is_super_admin,
+  created_at,
+  updated_at,
+  phone,
+  phone_confirmed_at,
+  phone_change,
+  phone_change_token,
+  phone_change_sent_at,
+  email_change_token_current,
+  email_change_confirm_status,
+  banned_until,
+  reauthentication_token,
+  reauthentication_sent_at)
+VALUES (
+  '00000000-0000-0000-0000-000000000000',
+  'f79a5ba6-7eaf-400e-8f70-c1e8cbd9631b',
+  'authenticated',
+  'authenticated',
+  'alice@example.com',
+  '$2a$10$mbTt9hIfVQK6mtjM0evvJOyO4QGFcfs2lW7cIdTtc7jxTDvXUE0rG',
+  '2022-08-05 14:06:38.556364+00',
+  NULL,
+  '', NULL, '',
+  NULL,
+  '', '',
+  NULL,
+  '2022-08-05 14:06:38.562479+00',
+  '{"provider": "email", "providers": ["email"]}',
+  '{}',
+  FALSE,
+  '2022-08-05 14:06:38.553239+00',
+  '2022-08-05 14:06:38.553242+00',
+  NULL,
+  NULL,
+  '', '',
+  NULL,
+  '', 0, NULL, '',
+  NULL);
+
+INSERT INTO auth.users (
+  instance_id,
+  id,
+  aud,
+  ROLE,
+  email,
+  encrypted_password,
+  email_confirmed_at,
+  invited_at,
+  confirmation_token,
+  confirmation_sent_at,
+  recovery_token,
+  recovery_sent_at,
+  email_change_token_new,
+  email_change,
+  email_change_sent_at,
+  last_sign_in_at,
+  raw_app_meta_data,
+  raw_user_meta_data,
+  is_super_admin,
+  created_at,
+  updated_at,
+  phone,
+  phone_confirmed_at,
+  phone_change,
+  phone_change_token,
+  phone_change_sent_at,
+  email_change_token_current,
+  email_change_confirm_status,
+  banned_until,
+  reauthentication_token,
+  reauthentication_sent_at)
+VALUES (
+  '00000000-0000-0000-0000-000000000000',
+  '1912ff43-e876-4961-9631-ecd09fe019bb',
+  'authenticated',
+  'authenticated',
+  'bob@example.com',
+  '$2a$10$GUruEatuNmaT7ftdyfLaf.aoerVJIdmfkYWLOP3zN7jiBxQBmQUsq',
+  '2022-08-05 14:06:47.337536+00',
+  NULL,
+  '', NULL, '',
+  NULL,
+  '', '',
+  NULL,
+  '2022-08-05 14:06:47.341379+00',
+  '{"provider": "email", "providers": ["email"]}',
+  '{}',
+  FALSE,
+  '2022-08-05 14:06:47.33564+00',
+  '2022-08-05 14:06:47.335643+00',
+  NULL,
+  NULL,
+  '', '',
+  NULL,
+  '', 0, NULL, '',
+  NULL);
+
+INSERT INTO auth.identities (
+  id,
+  user_id,
+  identity_data,
+  provider,
+  last_sign_in_at,
+  created_at,
+  updated_at)
+VALUES (
+  'f79a5ba6-7eaf-400e-8f70-c1e8cbd9631b',
+  'f79a5ba6-7eaf-400e-8f70-c1e8cbd9631b',
+  '{"sub": "f79a5ba6-7eaf-400e-8f70-c1e8cbd9631b"}',
+  'email',
+  '2022-08-05 14:06:38.555048+00',
+  '2022-08-05 14:06:38.555067+00',
+  '2022-08-05 14:06:38.555069+00');
+
+INSERT INTO auth.identities (
+  id,
+  user_id,
+  identity_data,
+  provider,
+  last_sign_in_at,
+  created_at,
+  updated_at)
+VALUES (
+  '1912ff43-e876-4961-9631-ecd09fe019bb',
+  '1912ff43-e876-4961-9631-ecd09fe019bb',
+  '{"sub": "1912ff43-e876-4961-9631-ecd09fe019bb"}',
+  'email',
+  '2022-08-05 14:06:47.33698+00',
+  '2022-08-05 14:06:47.337+00',
+  '2022-08-05 14:06:47.337002+00');
+
+-- Set up test user API token for quicker testing
+-- Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aWQiIDogImE3OTQxMDUyLTNkNWQtNDUwYS04YThlLTgzYzlmNjM5ODQ2YSIsICJpc3MiIDogInN1cGFiYXNlIiwgInN1YiIgOiAiZjc5YTViYTYtN2VhZi00MDBlLThmNzAtYzFlOGNiZDk2MzFiIiwgInJvbGUiIDogInRva2VuYXV0aGVkIiwgImlhdCIgOiAxNjYwMzk1ODA1LjIzMjEzNn0.CNf_cN1_rK77hAXnHsNr7wZB-XkThSGhp_d1RqWhIQI
+INSERT INTO api_auth.tokens (
+  id,
+  title,
+  user_id,
+  revoked,
+  created_at)
+VALUES (
+  'a7941052-3d5d-450a-8a8e-83c9f639846a',
+  'Test',
+  'f79a5ba6-7eaf-400e-8f70-c1e8cbd9631b',
+  FALSE,
+  '2022-08-08 17:49:35.722249+00');
+
