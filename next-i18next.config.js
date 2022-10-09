45a5d9e92f8ffbdc64351444a9d4bc09feb2b751
Felix Zedén Yverås <felix@fzy.se>
2022-10-09 00:01:24 +0000

    Convert translation files to yaml
    It will be necessary to have longer blocks of text in the translation
    files, sometimes including markdown. This is really hard to read and
    write in JSON whereas in YAML we can use multiline strings.
    
    To make things easier, this commit switches from json to yaml
    translation files.
    
    Note that `npm run dev` causes a warning:
    ```
    warn  - Invalid next.config.js options detected:
      - The root value has an unexpected property, localePath [...]
      - The root value has an unexpected property, localeExtension [...]
    ```
    However, these options are valid according to
    https://github.com/i18next/next-i18next and vercel does indeed have some
    problems with the translation files if `localePath` is not specified.

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "sv",
     locales: ["en", "sv"],
   },
+  localePath: path.resolve("./public/locales"),
+  localeExtension: "yaml",
 };
--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const { i18n } = require("./next-i18next.config");
+const i18nextConfig = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  ...i18nextConfig,
   reactStrictMode: true,
-  i18n,
   trailingSlash: true,
 };
 
