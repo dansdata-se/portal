0b0deb793480236a673ee2333d20d744cb982c20
Felix Zedén Yverås <felix@fzy.se>
2022-10-22 18:16:44 +0000

    Make images render in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,20 @@
+import * as NextImage from "next/future/image";
 import { I18nextProvider } from "react-i18next";
 import "../styles/globals.css";
 import i18n from "./i18n";
+
+// Fix images not rendering in storybook
+// https://dev.to/jonasmerlin/how-to-use-the-next-js-image-component-in-storybook-1415
+const OriginalNextImage = NextImage.default;
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage
+      {...props}
+      unoptimized
+    />
+  ),
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
