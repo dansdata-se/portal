0e55662582a6de8c938486d19c23a5d54a010ea6
Felix Zedén Yverås <felix@fzy.se>
2022-10-22 18:29:38 +0000

    Render yaml translations in Storybook
    Seems translations in storybook were broken by the switch to yaml-based
    translation files in 45a5d9e. Following this github answer:
    https://github.com/i18next/react-i18next/issues/1167#issuecomment-684824857
    I was able to restore them.

--- .storybook/i18n.js
+++ .storybook/i18n.js
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import Backend from "i18next-http-backend";
+import yaml from "js-yaml";
 import { initReactI18next } from "react-i18next";
 
 i18n
@@ -11,6 +12,12 @@ i18n
     fallbackLng: "sv",
     defaultNS: "common",
     supportedLngs: ["en", "sv"],
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.yaml",
+      parse: function (data) {
+        return yaml.load(data);
+      },
+    },
   });
 
 export default i18n;
