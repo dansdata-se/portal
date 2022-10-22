61b4957dad232b4e5611594f1f2041d604d2bc74
Felix Zedén Yverås <felix@fzy.se>
2022-10-22 17:25:19 +0000

    Fix issue where Language Button redirect away from the current page
    When the language button was pressed, the page url would be updated
    incorrectly, causing not only a redirect to the other language but also
    a redirect to another page entirely.

--- components/Button/Language/index.tsx
+++ components/Button/Language/index.tsx
@@ -1,6 +1,5 @@
 import Button, { ButtonAttributes } from "components/Button";
 import { GB as FlagGB, SE as FlagSE } from "country-flag-icons/react/3x2";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { ButtonHTMLAttributes } from "react";
 import { useTranslation } from "react-i18next";
@@ -14,19 +13,20 @@ function LanguageButton({
 }: Pick<ButtonAttributes, "variant"> &
   ButtonHTMLAttributes<HTMLButtonElement>) {
   const router = useRouter();
+  const { pathname, asPath, query } = router;
   const { t: tCommon } = useTranslation("common");
   return (
-    <Link
-      href="./"
-      locale={router.locale === "en" ? "sv" : "en"}
-    >
-      <Button
-        {...props}
-        variant={variant}
-        text={tCommon("change-locale")}
-        icon={router.locale === "en" ? <FlagSE /> : <FlagGB />}
-      />
-    </Link>
+    <Button
+      {...props}
+      variant={variant}
+      text={tCommon("change-locale")}
+      icon={router.locale === "en" ? <FlagSE /> : <FlagGB />}
+      onClick={() => {
+        router.push({ pathname, query }, asPath, {
+          locale: router.locale === "en" ? "sv" : "en",
+        });
+      }}
+    />
   );
 }
 
