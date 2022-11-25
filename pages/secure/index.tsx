f2886d396eb4f935d2b18a2dba058a51d192f109
Felix Zedén Yverås <felix@fzy.se>
2022-11-25 21:31:12 +0000

    fix: add temporary "sign out"/"delete account" buttons for signed in state
    This will need to be improved, not least "delete account" which I so far
    have not found a good way to handle, but is sufficient to allow testing
    for now.

--- pages/secure/index.tsx
+++ pages/secure/index.tsx
@@ -1,13 +1,19 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import Button from "components/Button";
 import LanguageButton from "components/Button/Language";
+import Cookies from "js-cookie";
+import Nav from "navigation/Nav";
 import type { NextPage } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { Database } from "types/supabase";
 
 const Home: NextPage = () => {
   const router = useRouter();
   const { t: tPage } = useTranslation("page-home");
+  const supabaseClient = useSupabaseClient<Database>();
   return (
     <div>
       <Head>
@@ -17,6 +23,38 @@ const Home: NextPage = () => {
       <main>
         <h1>Protected Area</h1>
         <LanguageButton variant={"text"} />
+        <Button
+          variant="outlined"
+          text="Logout"
+          onClick={() => {
+            supabaseClient.auth
+              .signOut()
+              .catch(console.error)
+              .then(() => {
+                router.push(Nav().Index.href);
+              });
+          }}
+        />
+        <Button
+          variant="outlined"
+          text="Delete Account"
+          onClick={() => {
+            supabaseClient.rpc("delete_account").then((r) => {
+              /* Temporary (bad) workaround to kill current session: */
+
+              if (r.error === null) {
+                // JWT is still techincally valid.
+                // Remove local cookie to prevent continued use of the site.
+                Cookies.remove("supabase-auth-token");
+                // Navigate user to the expected "signed out page"
+                router.push(Nav().Index.href);
+                // Force a page refresh to stop Next
+                // from using a cached `useSession` state.
+                window.location.reload();
+              }
+            });
+          }}
+        />
       </main>
     </div>
   );
