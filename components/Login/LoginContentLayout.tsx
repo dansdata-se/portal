ab089d749f6d747dfbf0ab8e605311c4ba9e1060
Felix Zedén Yverås <felix@fzy.se>
2022-11-25 17:53:07 +0000

    feat: make login page modular and implement email login

---
+++ components/Login/LoginContentLayout.tsx
@@ -0,0 +1,108 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import CircularProgressButton from "components/Button/Progress/Circular";
+import TextField from "components/TextField";
+import { useRef, useState } from "react";
+import { Eye, EyeOff } from "react-feather";
+import { TFunction, useTranslation } from "react-i18next";
+import { Database } from "types/supabase";
+import { useEventListener } from "usehooks-ts";
+
+function LoginContentLayout({ tPage }: { tPage: TFunction }) {
+  const { t: tCommon } = useTranslation("common");
+  const { t: tLoginCommon } = useTranslation("login/common");
+
+  const supabaseClient = useSupabaseClient<Database>();
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+
+  useEventListener("beforeunload", () => {
+    if (isLoading) {
+      return tCommon("onbeforeunload-generic-message");
+    }
+  });
+
+  function login() {
+    if (isLoading) return;
+
+    let isValid = true;
+    isValid = (emailRef.current?.reportValidity() ?? false) && isValid;
+    isValid = (passwordRef.current?.reportValidity() ?? false) && isValid;
+    if (!isValid) return;
+
+    const email = (emailRef.current as HTMLInputElement).value;
+    const password = (passwordRef.current as HTMLInputElement).value;
+
+    setLoading(true);
+    supabaseClient.auth
+      .signInWithPassword({
+        email,
+        password,
+      })
+      .then((response) => {
+        if (!response.error) return;
+
+        // Continue loading indication if login was successful
+        setLoading(false);
+
+        if (/e.?mail/i.test(response.error.message)) {
+          emailRef.current?.setCustomValidity(response.error.message);
+          emailRef.current?.reportValidity();
+        } else {
+          passwordRef.current?.setCustomValidity(response.error.message);
+          passwordRef.current?.reportValidity();
+        }
+      });
+  }
+
+  return (
+    <>
+      <h1>{tPage("page-title")}</h1>
+      <form
+        className="flex flex-col gap-4 w-full max-w-sm"
+        noValidate
+        onSubmit={(e) => {
+          e.preventDefault();
+          login();
+        }}
+      >
+        <TextField
+          ref={emailRef}
+          className="w-full"
+          variant="outlined"
+          label={tLoginCommon("label-email")}
+          type="email"
+          required
+        />
+        <TextField
+          ref={passwordRef}
+          className="w-full"
+          variant="outlined"
+          label={tLoginCommon("label-password")}
+          type={passwordVisible ? "singlelineText" : "password"}
+          trailing={
+            <button
+              type="button"
+              onClick={() => setPasswordVisible((visible) => !visible)}
+            >
+              {passwordVisible ? <EyeOff /> : <Eye />}
+            </button>
+          }
+          required
+        />
+        <CircularProgressButton
+          className="w-full"
+          variant="filled"
+          type="submit"
+          text={tLoginCommon("button-log-in")}
+          disabled={isLoading}
+          loading={isLoading}
+        />
+      </form>
+    </>
+  );
+}
+
+export default LoginContentLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,94 +1,34 @@
-import Button from "components/Button";
-import Footer from "components/Footer";
-import Header from "components/Header";
-import TextField from "components/TextField";
+import { useSession } from "@supabase/auth-helpers-react";
+import LoginLayout from "components/Login/Layout";
+import LoginContentLayout from "components/Login/LoginContentLayout";
 import Nav from "navigation/Nav";
 import type { GetServerSideProps, NextPage } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Image from "next/future/image";
 import Head from "next/head";
-import { useRef, useState } from "react";
-import { Eye, EyeOff } from "react-feather";
-import { useMediaQuery } from "usehooks-ts";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-const LogIn: NextPage = () => {
+const LoginPage: NextPage = () => {
   const { t: tPage } = useTranslation("page-log-in");
 
-  // Force small footer when image squishes the left-hand bar too much.
-  const forceSmallFooter = useMediaQuery(
-    "((min-width: 768px) and (max-width: 1150px)) or ((min-width: 1280px) and (max-width: 1700px))"
-  );
+  const router = useRouter();
+  const session = useSession();
 
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-  const [passwordVisible, setPasswordVisible] = useState(false);
+  useEffect(() => {
+    if (session !== null) {
+      router.push(Nav().secure.Index.href);
+    }
+  }, [router, session]);
 
   return (
     <>
       <Head>
         <title>{tPage("head-title")}</title>
       </Head>
-
-      <div className="flex min-h-full flex-row items-stretch">
-        <div className="relative flex min-h-full flex-col w-full md:w-1/2 xl:w-1/3">
-          <Header currentDestination={Nav().Login} />
-          <div className="h-8 grow"></div>
-          <main className="prose grow mx-auto px-4 w-full max-w-sm">
-            <h1>{tPage("title-log-in")}</h1>
-            <form
-              className="flex flex-col gap-4 w-full max-w-sm"
-              noValidate
-              onSubmit={(e) => {
-                e.preventDefault();
-                emailRef.current?.checkValidity();
-                passwordRef.current?.checkValidity();
-              }}
-            >
-              <TextField
-                ref={emailRef}
-                id="signin-email"
-                className="w-full"
-                variant="outlined"
-                label={tPage("label-email")}
-                type="email"
-                required
-              />
-              <TextField
-                ref={passwordRef}
-                id="signin-password"
-                className="w-full"
-                variant="outlined"
-                label={tPage("label-password")}
-                type={passwordVisible ? "singlelineText" : "password"}
-                trailing={
-                  <button onClick={() => setPasswordVisible(!passwordVisible)}>
-                    {passwordVisible ? <EyeOff /> : <Eye />}
-                  </button>
-                }
-                required
-              />
-              <Button
-                className="w-full"
-                variant="filled"
-                text={tPage("action-log-in")}
-                type="submit"
-              />
-            </form>
-          </main>
-          <div className="h-8 grow"></div>
-          <Footer forceSmall={forceSmallFooter} />
-        </div>
-        <div className="relative overflow-hidden flex-grow hidden md:block">
-          <Image
-            priority={true}
-            className="object-cover"
-            src="/images/dansbana.webp"
-            alt=""
-            fill
-          />
-        </div>
-      </div>
+      <LoginLayout currentDestination={Nav().Login}>
+        <LoginContentLayout tPage={tPage} />
+      </LoginLayout>
     </>
   );
 };
@@ -98,10 +38,11 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     props: {
       ...(await serverSideTranslations(context.locale as string, [
         "common",
+        "login/common",
         "page-log-in",
       ])),
     },
   };
 };
 
-export default LogIn;
+export default LoginPage;
