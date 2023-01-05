fca4cbd9aa87905b3aa50ae7919853ecab1318a9
Felix Zedén Yverås <felix@fzy.se>
2023-01-05 22:57:49 +0000

    feat: add padding and border radius values from MD3 docs
    Note: I found `Paddings.extraLarge=28` to be too large in some views. I
    therefore introduced `extraLarge=24` and `extraExtraLarge=28` instead.

---
+++ lib/app/theme/paddings.dart
@@ -0,0 +1,12 @@
+class Paddings {
+  static const none = 0.0;
+  static const extraSmall = 4.0;
+  static const small = 8.0;
+  static const medium = 12.0;
+  static const large = 16.0;
+  static const extraLarge = 24.0;
+  static const extraExtraLarge = 28.0;
+
+  static const betweenCards = small;
+  static const cardContent = large;
+}
---
+++ lib/app/theme/radii.dart
@@ -0,0 +1,25 @@
+/// https://m3.material.io/styles/shape/
+class Radii {
+  static const none = 0.0;
+  static const extraSmall = 4.0;
+  static const small = 8.0;
+  static const medium = 12.0;
+  static const large = 16.0;
+  static const extraLarge = 24.0;
+  static const extraExtraLarge = 28.0;
+
+  // https://m3.material.io/styles/shape/shape-scale-tokens
+  static const chip = small;
+  static const card = medium;
+  static const smallFab = medium;
+  static const extendedFab = large;
+  static const fab = large;
+  static const navigationDrawer = large;
+  static const bottomSheet = extraExtraLarge;
+  static const dialog = extraExtraLarge;
+  static const floatingSheet = extraExtraLarge;
+  static const largeFab = extraExtraLarge;
+  static const searchView = extraExtraLarge;
+  static const timePicker = extraExtraLarge;
+  static const timeInput = extraExtraLarge;
+}
