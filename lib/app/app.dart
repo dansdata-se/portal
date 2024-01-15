5ad0df1b7cbf7ad3223135d9c7db80804b54df29
Felix Zedén Yverås <felix@fzy.se>
2024-01-15 21:30:50 +0100

    feat: add temporary page title and introduction
    Just playing around a bit and seeing how this looks on an actual device.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -71,31 +71,64 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "You have pushed the button this many times:",
-            ),
+      body: LayoutBuilder(
+        builder: (context, constraints) => ListView(
+          padding: const EdgeInsets.all(8),
+          children: [
             Text(
-              "$_counter",
-              style: Theme.of(context).textTheme.headlineMedium,
+              "Dansdata",
+              style: Theme.of(context)
+                  .textTheme
+                  .displayLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.primary),
+            ),
+            const SizedBox(height: 8),
+            Text.rich(
+              TextSpan(
+                text:
+                    "Dansdata is a platform for the collection and distribution of Swedish dance information.\n\nBy facilitating the industry and making data freely available to developers, we want to encourage more people to take part in this wonderful interest: DANCING!\n\n",
+                style: Theme.of(context).textTheme.bodySmall!,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: "In the swing with swedish dance",
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontStyle: FontStyle.italic,
+                        ),
+                  ),
+                ],
+              ),
+            ),
+            ConstrainedBox(
+              constraints: BoxConstraints.tightFor(
+                height: constraints.maxHeight - 8,
+              ),
+              child: Center(
+                // Center is a layout widget. It takes a single child and positions it
+                // in the middle of the parent.
+                child: Column(
+                  // Column is also a layout widget. It takes a list of children and
+                  // arranges them vertically. By default, it sizes itself to fit its
+                  // children horizontally, and tries to be as tall as its parent.
+                  //
+                  // Column has various properties to control how it sizes itself and
+                  // how it positions its children. Here we use mainAxisAlignment to
+                  // center the children vertically; the main axis here is the vertical
+                  // axis because Columns are vertical (the cross axis would be
+                  // horizontal).
+                  //
+                  // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+                  // action in the IDE, or press "p" in the console), to see the
+                  // wireframe for each widget.
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text("You have pushed the button this many times:"),
+                    Text(
+                      "$_counter",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
@@ -104,7 +137,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: "Increment",
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
